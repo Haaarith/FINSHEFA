e7c2c4b79b30991a069fce13eec0c04d6843bfd7
Harith <62058475+Haaarith@users.noreply.github.com>
2024-10-02 23:17:00 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -246,5 +246,4 @@ def download_file():
 
 # Run Flask on port 80 for HTTP access (might need sudo for permission)
 if __name__ == '__main__':
-    context = ('/etc/letsencrypt/live/harithx.com/fullchain.pem', '/etc/letsencrypt/live/harithx.com/privkey.pem')
-    app.run(host='0.0.0.0', port=443, ssl_context=context)
+    app.run(host='127.0.0.1', port=5000)
