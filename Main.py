9e6afccb84519dce7d7faa44ba7064980c920d0b
Harith <62058475+Haaarith@users.noreply.github.com>
2025-07-01 11:33:47 +0300

    add != failed to the missing from hyper

--- Main.py
+++ Main.py
@@ -79,7 +79,7 @@ def compare_transactions(azm_df, hyperpay_df):
             if row['_merge'] == 'right_only' and row['Result'] != 'NOK':
                 # Missing from AZM
                 missing_from_azm = pd.concat([missing_from_azm, row[['TransactionId', 'Credit', 'RequestTimestamp', 'Result']].to_frame().T], ignore_index=True)
-            elif row['_merge'] == 'left_only' and row['حالة العملية'] != 'rejected' and row['حالة العملية'] !='time_out':
+            elif row['_merge'] == 'left_only' and row['حالة العملية'] != 'rejected' and row['حالة العملية'] !='time_out' and row['حالة العملية'] != 'failed':
                 # Missing from HyperPay
                 missing_from_hyperpay = pd.concat([missing_from_hyperpay, row[['تاريخ العملية', 'حالة العملية', 'تفاصيل العملية (رقم الحوالة)', 'وسيلة الدفع', 'المبلغ (ريال)']].to_frame().T], ignore_index=True)
             elif row['_merge'] == 'both' and row['Result'] == 'ACK' and row['حالة العملية'] != 'success':
